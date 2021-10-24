911e3f17b4c2311d2621c8536b6d234d145ee394
Maksym-Maleiev <maximmaleev93@gmail.com>
2021-10-24 14:41:53 +0300

    js

---
+++ javascript.js
@@ -0,0 +1,33 @@
+// Find middle character of the word.
+// User is going to input a word. Your task is to find the middle character of this word.
+// If the word’s length is odd - return the middle character.If word’s length is even - return the middle 2 characters.
+// You should validate for empty value and for value with spaces only, in that case show message: “Invalid value”.
+// For user input use “prompt” function. For displaying result use “alert” function.
+
+// Examples:
+// 	For user input “test” should return “es”
+// 	For user input “character” should return “a”
+// 	For user input “B” should return “B”
+// 	For user input “” should return “Invalid value”
+// For user input “       “ should return “Invalid value”
+
+const word = prompt("Please enter a word");
+console.log(word);
+let message;
+
+if (word === null || word.trim() === "") {
+    message = "Invalid value";
+    alert(message);
+// } else if (word.trim() === "") {
+//     message = "Invalid value";
+//     alert(message);
+// } else if (word === "") {
+//     message = "Invalid value";
+//     alert(message);
+} else {
+    if (word.length % 2 === 0) {
+       alert(`${word[word.length / 2 - 1] + word[word.length / 2]}`);
+    } else {
+        alert(`${word[Math.floor(word.length / 2)]}`);
+    }
+}
